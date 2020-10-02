564d85f9f3904cc2e8a6dc87f29097c9228a9db4
Jim Read <jim.read@gmail.com>
2020-10-02 04:44:38 +0000

    terminal/gui app

--- classes.py
+++ classes.py
@@ -17,11 +17,13 @@ def cls():
 #progress bar decorator for gui
 def progress_bar_decorator(func):
     def inner(*args, **kwargs):
-        for i in range(1, 500):
-            sg.one_line_progress_meter('API calls', i+1, 500, 'key','Retrieving Information ... ', (50,50), orientation='h')
+        for i in range(1, 300):
+            sg.one_line_progress_meter('API calls', i+1, 300, 'key','Retrieving Information ... ', (50,50), orientation='h')
         returned = func(*args, **kwargs)
         return returned
     return inner
+
+
 
 ''' Menu class used to drive the application and have other features utilize class functions and return parameters as such and correct table '''
 
@@ -31,6 +33,7 @@ class Iss:
 
     @staticmethod
     def iss_current_co_ords():
+
         ''' Get response from API and make it readible in python via JSON, create a new list from dict value and
         extract the information needed and store in a new dictionary and return as parameters'''
 
@@ -40,7 +43,8 @@ class Iss:
             response = requests.get(url_for_current_position)
             iss_info = response.json()
         except:
-            input('\nThe API is currently down - Press try again later')
+            print('\nThe API is currently down - Press try again later')
+
         # create new dictionary with required values needed from API json response
         convert_list = [value for key, value in iss_info.items() if key == 'iss_position']
         parameters = {}
@@ -56,15 +60,15 @@ class Iss:
 
     def get_timezone_location(parameters):
         url = f"http://api.geonames.org/timezoneJSON?formatted=true&lat={parameters['latitude']}&lng={parameters['longitude']}&username=coder_academy_jim"
-        response = (requests.get(url)).json()  ## Make a request and convert from json to dictionary
-
+        response = (requests.get(url)).json()  
+        ## Make a request and convert from json to dictionary
         for key, value in response.items():
 
-            if key == 'timezoneId':  # {
+            if key == 'timezoneId':      # {
                 parameters[key] = value  # 'timezoneId': 'Australia/Melbourne',
-            if key == 'time':  # 'countryName': 'Australia',
+            if key == 'time':            # 'countryName': 'Australia',
                 parameters[key] = value  # 'time': '2020-09-25 00:19'
-            if key == 'countryName':  # }
+            if key == 'countryName':     # }
                 parameters[key] = value
 
             elif len(response) == 5: # key !='timezoneId' or 'timecountry' or 'name':
@@ -75,13 +79,15 @@ class Iss:
 
 
     def location_of_iss_at_input(parameters):
-        ''' Checking to see if iSS over land or water, if over land a location key is given and name key is given if over water'''
+
+        ''' Checking to see if iSS over land or water, if over land a location key is given and name key is given if over water '''
+
         #create new dictionary and add lat/long and location key if over land - return as variable
         try:                                                                                             # {
             geolocator = Nominatim(user_agent="check_if_over_water_or_not")                              # 'longitude': '22.4747',
             location = geolocator.reverse(f"{parameters['latitude']}, {parameters['longitude']}")        # 'latitude': '10.8322',
-            parameters['location'] = location                                                         # 'location': Location(Vakaga,(9.8250502, 22.376363, 0.0))
-            parameters['longitude'] = parameters['longitude']                                         # }
+            parameters['location'] = location                                                            # 'location': Location(Vakaga,(9.8250502, 22.376363, 0.0))
+            parameters['longitude'] = parameters['longitude']                                            # }
             parameters['latitude'] = parameters['latitude']
             return parameters
 
@@ -104,6 +110,8 @@ class Iss:
             return parameters
 
 
+    ''' Get user input - check location with GEOPY and return dictionary '''
+
     def get_user_location():
         parameters = {}
         geolocator = Nominatim(user_agent="test")
@@ -122,12 +130,14 @@ class Iss:
                 cls()
         return parameters
 
+
+    ''' Same thing for terminal app - get users location but in gui that requres a pop up as there is no term window takes address arguement '''
+
     def get_user_location_gui_popup(address):
         parameters = {}
         geolocator = Nominatim(user_agent="test")
         while_true = True
         while while_true:
-            #address = input('\nEnter an address, suburb, town, location: ')
             cls()
             try:
                 location = geolocator.geocode(address)  # {
@@ -140,6 +150,8 @@ class Iss:
                 break
         return parameters
 
+
+    ''' checks the users location input and iss and then checks the approx pass time '''
 
     def get_pass_times_at_location(parameters):
         iss_co_ords = Iss.iss_current_co_ords()
@@ -158,6 +170,9 @@ class Iss:
         parameters['times'] = times
         parameters['iss'] = iss_location_with_timezone
         return parameters
+
+
+    ''' Takes user input - and checks the iss adn runs another call through geopy geodesic to get distance '''
 
 
     def measure_distance(parameters):
@@ -187,6 +202,7 @@ class Iss:
 
 
 
+''' Astronaut class to call astros endpoint and return dictinary results and return them on terminal or gui'''
 
 class Astronaut:
 
@@ -233,15 +249,20 @@ class Astronaut:
 
 
 
+''' Tables created with Rich, variables are called and these are used to display information to the terminal only '''
+
 
 class Display_tables:
 
+    ''' returns the current location of iss table '''
+
     def return_iss_location():
+        #get informaiton about iss
         iss_co_ords = Iss.iss_current_co_ords()
         iss_co_ords_with_location = Iss.location_of_iss_at_input(iss_co_ords)
         iss_location_with_timezone = Iss.get_timezone_location(iss_co_ords_with_location)  # adds timezone if availe to iss dict
 
-
+        #build table and populate 
         table = Table(title="Current ISS location")
         table.add_column("Time Zone", justify="center", style="cyan", no_wrap=True)
         table.add_column(f"Location", justify="center", style="magenta")
@@ -253,11 +274,10 @@ class Display_tables:
                       f"{iss_location_with_timezone['latitude']}", f"{iss_location_with_timezone['longitude']}")
 
         console = Console()
-        console.print(table)
-        #return console.print(table)
+        return console.print(table)
 
 
-
+    ''' returns the current pass times of iss table at user location '''
 
     def display_pass_times_at_location():
 
@@ -289,11 +309,10 @@ class Display_tables:
                           f"{user_loc_with_timez['longitude']}")
 
         console = Console()
-        console.print(table)
-        return Menu.menu()
+        return console.print(table)
 
 
-
+    ''' returns the current distance table of location to iss '''
 
     def display_distance():
 
@@ -322,6 +341,9 @@ class Display_tables:
         console = Console()
         return console.print(table)
 
+
+    ''' returns the current location of iss for the gui application '''
+
     @progress_bar_decorator
     def gui_iss_info():
         iss_co_ords = Iss.iss_current_co_ords()
@@ -331,13 +353,17 @@ class Display_tables:
 
 
 
-
+''' GUI class to run the front end and make calls to the ISS and Astronaut class '''
 
 class Gui:
 
+''' Create the GUI Front end and populate buttons, fields etc '''
 
     @progress_bar_decorator
     def make_gui():
+
+        #create theme, layout and define window propteries
+
         sg.theme('Darkblack')
         tab1_layout =  [[sg.T('ISS',)], [sg.T('Location:', size=(8,1)), sg.T('',key='-iss_location-', size=(80,1),background_color='white', text_color='black')], [sg.T('Country:', size=(8,1)), sg.T('',key='-iss_countryName-', size=(80,1),background_color='white', text_color='black')], [sg.T('Time Zone:'), sg.T('',key='-iss_timezoneId-', size=(80,1),background_color='white', text_color='black')], [sg.T('Time/Date:'), sg.T('',key='-iss_time-', size=(80,1),background_color='white', text_color='black')], [sg.T('Latitude:', size=(8,1)), sg.T('', size=(8,1), key='-iss_lat-',background_color='white', text_color='black'),sg.T('Longitude', size=(8,1)), sg.T('', size=(7,1), key='-iss_lon-',background_color='white', text_color='black')], [sg.T(''), sg.T('')], [sg.T('Input Location:')], [sg.T('Address:', size=(8,1)), sg.T('', size=(80,1), key='-local_location-',background_color='white', text_color='black')], [sg.T('Country:', size=(8, 1)), sg.T('', size=(80,1),key='-local_countryName-',background_color='white', text_color='black')], [sg.T('Time Zone:', size=(8,1)), sg.T('', size=(80,1),key='-local_timezoneId-',background_color='white', text_color='black')], [sg.T('Time/Date', size=(8,1)), sg.T('', size=(80,1),key='-local_time-',background_color='white', text_color='black')], [sg.T('Latitude:', size=(8,1)), sg.T('', size=(7,1),key='-local_latitude-',background_color='white', text_color='black'),sg.T('Longitude', size=(7,1)), sg.T('', size=(7,1),key='-local_longitude-',background_color='white', text_color='black')], [sg.T('Distance', size=(8,1)), sg.T('', key="-iss_distance-", size=(10,1),background_color='white', text_color='black')], [sg.Image(filename='../tttttttttttttttttttttt/HDEV3.png', size=(1000, 350))]]
 
@@ -347,16 +373,15 @@ class Gui:
 
         window = sg.Window('My window with tabs', layout, default_element_size=(80,800))
 
+        #create loop that constantly checks for user input/button presses/etc
+
         while True:
             event, values = window.read()
             if event in (sg.WIN_CLOSED, 'Exit'):
-                window.close()
+                window.close()  # destoy window and break loop - return to terminal menu
                 break
-            # if event == "Exit":
-            #     window.close()
-            #     break
 
-            if event == 'ISS':
+            if event == 'ISS':  # update all fields on screen - keys match text outputs
                 iss_info = Display_tables.gui_iss_info()
                 window['-iss_location-'].update(f"{iss_info['location']}")
                 window['-iss_countryName-'].update(f"{iss_info['countryName']}")
@@ -366,7 +391,7 @@ class Gui:
                 window['-iss_lon-'].update(f"{iss_info['longitude']}")
 
 
-            if event == 'Passes':
+            if event == 'Passes':  # pop asks user for input - checked and updates all fields - pop up times
                 geolocator = Nominatim(user_agent="test")
                 address = sg.popup_get_text('Enter an address, suburb, town, location')
                 try:
@@ -389,16 +414,16 @@ class Gui:
                     window['-iss_time-'].update(f"{time_zones['iss']['time']}")
                     window['-iss_lat-'].update(f"{time_zones['iss']['latitude']}")
                     window['-iss_lon-'].update(f"{time_zones['iss']['longitude']}")
-
+                    #after all fields on scren are updated, pass times pops up on screen
                     sg.popup(f'\nThe next 5 Passes at Location:\n\n1st -  {time_zones["times"][0]}\n2nd - {time_zones["times"][1]}\n3rd -  {time_zones["times"][2]}\n4th -  {time_zones["times"][3]}\n5th -  {time_zones["times"][4]}\n\n', title="Next 5 Passes", line_width=120)
                     window['-iss_distance-'].update(f"{distance['distance']} kms")
-                except:
+                except:  #sometimes some locations have 4 times - usualy because the iSS just passed recently
                     window['-iss_distance-'].update(f"{distance['distance']} kms")
                     sg.popup(f'\n          The next 4 Passes at Location:\n1st -  {time_zones["times"][0]}\n2nd - {time_zones["times"][1]}\n3rd -  {time_zones["times"][2]}\n4th -  {time_zones["times"][3]}', title="Next 4 Passes", line_width=120)
 
 
 
-            if event == 'Measure':
+            if event == 'Measure':  #pop up asks for user input - checks this - gets iss information and measures - updates all fields on the screen
                 geolocator = Nominatim(user_agent="test")
                 address = sg.popup_get_text('Enter an address, suburb, town, location')
                 try:
@@ -428,7 +453,7 @@ class Gui:
 
 
 
-            if event == 'Astronauts':
+            if event == 'Astronauts':  # Astros endpoint is called and a pop up displays who is on board
                 try:
                     astros_on_iss = Astronaut.gui_astros()
                     sg.popup(f'Astronauts onboard ISS\n{astros_on_iss}', title="Astronauts", line_width=150)
@@ -436,12 +461,18 @@ class Gui:
                     sg.popup_error('There was no connection to API')
 
 
-            if event == "Help":
-                with open('../tttttttttttttttttttttt/help.txt', 'r') as file:
+            if event == "Help":  # Help file is called and a pop up with the contents is displayed
+                with open('help.txt', 'r') as file:
                     help_file = file.read()
                 sg.popup(f'{help_file}', title="Help", line_width=150)
 
-            if event == "Clear":
+            if event == "About":  # ABout file is called and a pop up with the contents is displayed
+                with open('about.txt', 'r') as file:
+                    about_file = file.read()
+                sg.popup(f'{about_file}', title="Help", line_width=150)
+            
+
+            if event == "Clear":  # Clear event will remove all values on the screen to empty strings
                 window['-local_location-'].update("")
                 window['-local_countryName-'].update("")
                 window['-local_timezoneId-'].update("")
@@ -460,12 +491,12 @@ class Gui:
 
 
 
-
-
-
+''' Menu class used to drive the terminal app, calls are made to ISS class to get data like object through dict of operations '''
 
 
 class Menu:
+
+''' create a dictionary of operation for the user to select '''
 
     def __init__(self):
         self.operations = {
@@ -474,14 +505,17 @@ class Menu:
             'get': Display_tables.display_pass_times_at_location,
             'measure': Display_tables.display_distance,
             'help': Menu.help_text,
+            'about': Menu.help_text,
             'gui': Gui.make_gui }
 
+
+''' define the actual persistant menu that will be underneath all applications - even when gui is quit - returns to this - 1-8 selections '''
 
 
     def menu(self):
         print('\n')
         menu_start = True
-        while menu_start:
+        while menu_start:  # Draw menu to screen via rich tables
             table = Table(title="")
 
             table.add_column("Menu", justify="left", style="magenta")
@@ -492,8 +526,8 @@ class Menu:
 
             operation = input('Select an option: ')
 
-            try:
-                if operation.strip() == '1':
+            try:  # Try all selections and if any thing is wrong - catch all - repeat menu
+                if operation.strip() == '1':  #  option gets the relevant object and then calls on it when needed
                     current = self.operations.get('current')
                     current()
                 elif operation.strip() == '2':
@@ -509,9 +543,8 @@ class Menu:
                     help = self.operations.get('help')
                     help()
                 elif operation.strip() == '6':
-                    # about = self.operations.get('about')
-                    # about()
-                    pass
+                    about = self.operations.get('about')
+                    about()
                 elif operation.strip() == '7':
                     exit()
                 elif operation.strip() == '8':
@@ -524,10 +557,20 @@ class Menu:
             except:
                 Menu.menu(self)
 
+    ''' Help and ABout files needd to be displayed to window '''
+
     @staticmethod
     def help_text():
         cls()
-        with open('../tttttttttttttttttttttt/help_file_gui.txt', 'r') as help_file:
+        with open('help_file.txt', 'r') as help_file:
+            print(help_file.read())
+        input('Press enter')
+        return Menu.menu()
+
+    @staticmethod
+    def about_text():
+        cls()
+        with open('about.txt', 'r') as help_file:
             print(help_file.read())
         input('Press enter')
         return Menu.menu()
