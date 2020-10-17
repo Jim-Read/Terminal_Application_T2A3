b984a4bc60ae7dad33d5caba35aa7c1b3dbf5c4a
Jim-Read <66547446+Jim-Read@users.noreply.github.com>
2020-10-18 05:29:23 +1100

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python
 from classes import Menu, Gui
-from rich.progress import Progress
-import os, sys, time
+import os, sys
 
+#create menu object
+menu = Menu()
 
+#give user sys args options - load gui or help then into terminal or just into terminal with no args
 if "--help" in sys.argv:
-    input("\nSee the help file in the menu for more information about the application \n\nUsing --gui will launch the gui front end instead\nUsage: python main.py --gui \n\n- Press enter")
+    input("\nSee the help file in the menu for more information about the application \n\n"
+          "Using --gui will launch the gui front end instead\nUsage: python main.py --gui \n\n- Press enter")
 if '--gui' in sys.argv:
     Gui.make_gui()
 
+#calls menu object
+menu.menu()
 
-
-self = Menu()
-self.menu()
